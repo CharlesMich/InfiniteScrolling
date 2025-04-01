34ab9101b2df293a6b6d0a4a3278bdfa7f31f80a
CharlesMich <charleedongo@hotmail.com>
2025-04-01 17:12:51 -0500

    Initial Commit

---
+++ InfiniteScrolling/InfiniteScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollingApp.swift
+//  InfiniteScrolling
+//
+//  Created by Charles Michael on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
